951be6fb5d1ff1d9149668d7e06abf88e1093c72
Sushant Sharma <sushant20.sharma00@gmail.com>
2023-09-23 12:46:22 +0530

    changes made

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
-// app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
+app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
